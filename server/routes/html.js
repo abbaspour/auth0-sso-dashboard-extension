ab1c44717732c0db559bd0f3e3465501b9784c83
Amin Abbaspour <amin.abbaspour@auth0.com>
2018-03-05 12:28:43 +1100

    using rawgit

--- server/routes/html.js
+++ server/routes/html.js
@@ -20,7 +20,7 @@ export default () => {
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
     <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>" /><% } %>
-    <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="https://raw.githubusercontent.com/abbaspour/auth0-sso-dashboard-extension/master/dist/auth0-sso-dashboard-authz.ui.<%= assets.version %>.css" /><% } %>
+    <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/abbaspour/auth0-sso-dashboard-extension/67a67447/dist/auth0-sso-dashboard-authz.ui.<%= assets.version %>.css" /><% } %>
     <% if (assets.customCss) { %><link rel="stylesheet" type="text/css" href="<%= assets.customCss %>" /><% } %>
   </head>
   <body>
@@ -31,8 +31,8 @@ export default () => {
     <% if (assets.vendors) { %><script type="text/javascript" src="<%= assets.vendors %>"></script><% } %>
     <% if (assets.app) { %><script type="text/javascript" src="<%= assets.app %>"></script><% } %>
     <% if (assets.version) { %>
-    <script type="text/javascript" src="https://raw.githubusercontent.com/abbaspour/auth0-sso-dashboard-extension/master/dist/auth0-sso-dashboard-authz.ui.vendors.<%= assets.version %>.js"></script>
-    <script type="text/javascript" src="https://raw.githubusercontent.com/abbaspour/auth0-sso-dashboard-extension/master/dist/auth0-sso-dashboard-authz.ui.<%= assets.version %>.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/abbaspour/auth0-sso-dashboard-extension/67a67447/dist/auth0-sso-dashboard-authz.ui.vendors.<%= assets.version %>.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/abbaspour/auth0-sso-dashboard-extension/67a67447/dist/auth0-sso-dashboard-authz.ui.<%= assets.version %>.js"></script>
     <% } %>
   </body>
   </html>
